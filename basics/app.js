384006d399e2a95b1ef2a8411bb134e9d44313a8
Brandon Dalton <dabigin@gmail.com>
2022-11-13 20:39:25 -0600

    Section 20 Lection 205 Our Very First Function

---
+++ basics/app.js
@@ -0,0 +1,10 @@
+//DEFINE FUNCTION
+// DEFINE FUNCTIONS BEFORE CALLING THEM
+function singSong() {
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
+}
+// CALL FUNCTION
+//
+singSong();
