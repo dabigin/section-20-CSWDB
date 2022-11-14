aee0b08de2124bf6760a1e54192dcbac2147ec5c
Brandon Dalton <dabigin@gmail.com>
2022-11-13 21:13:54 -0600

    Section 20 Lecture 207 Functions with Multiple Arguments

--- basics/app.js
+++ basics/app.js
@@ -9,6 +9,22 @@
 //
 // singSong();
 
-function greet(firstName) {
-  console.log(`Hey there, ${firstName}`);
+// Arguments Intro
+// function greet(firstName) {
+//   console.log(`Hey there, ${firstName}`);
+// }
+
+// Functions with multiple arguements
+function greet(firstName, lastName) {
+  console.log(`Hey there, ${firstName} ${lastName[0]}.`);
+}
+
+greet("George", "Clooney");
+
+function repeat(str, numTimes) {
+  let result = "";
+  for (let i = 0; i < numTimes; i++) {
+    result += str;
+  }
+  console.log(result);
 }
